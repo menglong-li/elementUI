f1d04bca683fca45d744753a0b5f5f7eee721a13
Menglong.Li <116938310@qq.com>
2019-07-15 23:05:19 +0800

    put,get

--- mock/mock.js
+++ mock/mock.js
@@ -2,6 +2,7 @@ import Mock from 'mockjs';
 
 var https = 'http://localhost:8090/api';
 
+//#region 登录
 var LoginIn = Mock.mock({
     'id': 1,
     'time': Mock.Random.datetime('yyyy-MM-dd HH:mm:ss'),
@@ -12,16 +13,48 @@ var LoginIn = Mock.mock({
 Mock.mock(RegExp(https + '/Login/in.*'),'get', options => {
     return LoginIn;
 });
+//#endregion
 
+//#region 新闻列表
+
+/**
+ * 新闻数据
+ */
 var newslist = Mock.mock({
     'list|1-20':[{
         'id|+1':1,
         'title':Mock.Random.cparagraph(1,3)
     }]
 });
-
+/**
+ * 新闻api监听
+ */
 Mock.mock(RegExp(https + '/news/getlist'),'get',options => {
     return newslist;
 })
+//#endregion
+
+//#region 参数设置
+var webset = {
+    title: '初始化',
+    keys: '',
+    des: '',
+}
+
+/**
+ * 请求数据
+ */
+Mock.mock(RegExp(https + '/setting/getweb'),'get', options => {
+    return webset;
+})
+
+/**
+ * 更新
+ */
+Mock.mock(RegExp(https + '/setting/put'), 'put',options => {
+    webset = JSON.parse(options['body']);
+    return webset;
+})
+//#endregion
 
 export default Mock;
---
+++ src/views/setting/web.vue
@@ -0,0 +1,71 @@
+<template>
+    <el-form :model="rule" :rules="rules" ref="forms" class="demo-ruleForm globalForm" label-width="100px">
+        <el-form-item label="网站标题" prop="title">
+            <el-input placeholder="请输入网站标题" v-model="rule.title" clearable></el-input>
+        </el-form-item>
+        <el-form-item label="关键词" prop="keys">
+            <el-input placeholder="请输入关键词" v-model="rule.keys" clearable></el-input>
+        </el-form-item>
+        <el-form-item label="网站描述" prop="des">
+            <el-input type="textarea" placeholder="请输入网站描述" v-model="rule.des" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="submit">提交</el-button>
+            <el-button @click="reset">重置</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'web',
+                rule: {
+                    title: '',
+                    keys: '',
+                    des: '',
+                },
+                rules: {
+                    title: [
+                        { required: true, message: '不能为空' }
+                    ],
+                    keys: [
+                        { required: true, message: '不能为空' }
+                    ],
+                    des: [
+                        { required: true, message: '不能为空' }
+                    ]
+                },
+            }
+        },
+        methods: {
+            submit() {
+                this.$refs['forms'].validate(valid => {
+                    if(valid) {
+                        let params = {
+                            params: {
+                                title: this.rule.title,
+                                keys: this.rule.keys,
+                                des: this.rule.des,
+                            }
+                        }
+                        this.$http.put('/api/setting/put',this.rule).then(data => {
+                            console.log(data);
+                        }).catch(error => {
+                            console.log(error);
+                        })
+                    }
+                });
+            },
+            reset() {
+                this.$refs['forms'].resetFields();
+            }
+        },
+        created() {
+            this.$http.get('/api/setting/getweb').then(data => {
+                this.rule = data['data'];
+            })
+        },
+    }
+</script>
