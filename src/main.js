667539622bdd9106db507ea145c8685c61224d93
Menglong.Li <116938310@qq.com>
2020-09-14 01:45:43 +0800

    增加文本编辑器

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import './plugins/element.js'
 import router from './router'
 import axios from './api'
 import store from './store/store.js'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+/*editor */
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+Vue.use(VueQuillEditor);
+/**End:editor */
 
 if (process.env.VUE_APP_Mock == true) {    // 判断是否为mock模式
   require('../mock/mock.js');
--- src/views/goods/sale/edit.vue
+++ src/views/goods/sale/edit.vue
@@ -11,48 +11,33 @@
         <el-form-item label="标题" prop="title">
             <el-input placeholder="请输入标题" v-model="rule.title" clearable></el-input>
         </el-form-item>
-        <el-form-item label="原价" prop="originalprice">
-            <el-input placeholder="请输入原价" type="Number" oninput="value=(value.match(/\d+(\.\d{0,2})?/)||[''])[0]" v-model="rule.originalprice" class="inputNumber" clearable></el-input>
-        </el-form-item>
         <el-form-item label="单价" prop="price">
             <el-input placeholder="请输入单价" type="Number" oninput="value=(value.match(/\d+(\.\d{0,2})?/)||[''])[0]" v-model="rule.price" class="inputNumber" clearable></el-input>
         </el-form-item>
         <el-form-item label="库存量" prop="inventory">
             <el-input placeholder="请输入库存量" type="Number" oninput="value=value.replace(/\-/g,'')" v-model="rule.inventory" class="inputNumber" clearable></el-input>
         </el-form-item>
-        <el-form-item label="返还积分" prop="integral">
-            <el-input placeholder="请输入返还积分" type="Number" oninput="value=value.replace(/\-/g,'')" v-model="rule.integral" class="inputNumber" clearable></el-input>
-        </el-form-item>
         <el-form-item label="序号" prop="sort">
             <el-input placeholder="请输入序号" type="Number" oninput="value=value.replace(/\-/g,'')" v-model="rule.sort" class="inputNumber" clearable></el-input>
         </el-form-item>
-        <el-form-item label="支付方式">
-            <el-checkbox-group v-model="rule.isintegral">
-                <el-checkbox label="现金" name="0"></el-checkbox>
-                <el-checkbox label="积分" name="1"></el-checkbox>
-            </el-checkbox-group>
-        </el-form-item>
         <el-form-item label="立即上架" prop="isSale">
-            <el-switch v-model="rule.isSale"></el-switch>
+            <el-switch v-model="rule.isSale"
+            :active-value="1"
+            :inactive-value="0"
+            ></el-switch>
         </el-form-item>
-        <el-form-item>
-            <el-upload
-                name="photo"
-                :action="uploadUrl"
-                :headers="token"
-                :multiple="true"
-                :limit="20"
-                list-type="picture-card"
-                :on-preview="handlePictureCardPreview"
-                :on-error="uploadError">
-                <i class="el-icon-plus"></i>
-            </el-upload>
-            <el-dialog :visible.sync="dialogVisible">
-                <img width="100%" :src="dialogImageUrl" alt="">
-            </el-dialog>
+        <el-form-item label="商品图片">
+            <Resource @imglist="setImgArray"></Resource>
         </el-form-item>
         <el-form-item label="内容" prop="contents">
-            <el-input placeholder="请输入库存量" v-model="rule.contents" clearable></el-input>
+            <quill-editor class="ql-editor-class"
+                v-model="rule.contents"
+                ref="myQuillEditor"
+                :options="editorOption"
+                @focus="onEditorFocus($event)"
+                @blur="onEditorBlur($event)"
+                @change="onEditorChange($event)">
+            </quill-editor>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="submit">提交</el-button>
@@ -62,40 +47,37 @@
 </template>
 
 <script>
+import Resource from '@/components/Resource';
+
     export default {
         data() {
             return {
                 name: 'edit',
-                uploadUrl: process.env.VUE_APP_API + '/api/product/uploadimg',
-                token: {
-                    Authorization: this.$store.state.token
-                },
                 options: [],
-                dialogImageUrl: '',
-                dialogVisible: false,
+                editorOption: {},
                 rule: {
                     id: 0,
                     tid: '',
                     title: '',
-                    originalprice: null,
                     price: null,
                     inventory: null,
-                    integral: null,
                     sort: 0,
-                    isintegral: [],
-                    isSale: true,
+                    isSale: 1,
                     contents: '',
-                    photo: []
+                    photo: ''
                 },
                 rules: {
                     title: { required: true, message: '请输入标题' },
-                    originalprice: {required: true, message: '请输入原价'},
                     price: {required: true, message: '请输入单价'},
                     inventory: {required: true, message: '请输入库存量'},
-                    integral: {required: true, message: '请输入返还积分'},
                     sort: {required: true, message: '请输入序号'},
                 },
             }
+        },
+        computed: {
+            editor() {
+                return this.$refs.myQuillEditor.quill;
+            },
         },
         created() {
             this.getType();
@@ -113,16 +95,6 @@
                 //分类初始化赋值
                 await this.$http.get('/api/producttype/getlist').then(results => {
                     this.options = this.convertToTreeData(results['data'],0,0);
-                });
-            },
-            handlePictureCardPreview(file) {
-                this.dialogImageUrl = file.url;
-                this.dialogVisible = true;
-            },
-            uploadError(err, file, fileList) {
-                this.$message({
-                    message:err,
-                    type: 'error'
                 });
             },
             convertToTreeData(data, pid,index) {
@@ -150,12 +122,20 @@
                 this.$refs['forms'].validate(valid => {
                     if(valid) {
                         if(this.rule.id == 0) {
-                            this.$http.post('/api/admin/register',this.rule).then(() => {
-                                this.$router.push('/setting/admin');
+                            this.$http.post('/api/product/add',this.rule).then(() => {
+                                this.$message({
+                                    message: '操作成功',
+                                    type: 'success'
+                                });
+                                this.$router.push('/goods/sale');
                             })
                         }else {
-                            this.$http.put('/api/admin/edit',this.rule).then(() => {
-                                this.$router.push('/setting/admin');
+                            this.$http.put('/api/product/edit',this.rule).then(() => {
+                                this.$message({
+                                    message: '操作成功',
+                                    type: 'success'
+                                });
+                                this.$router.push('/goods/sale');
                             }).catch(()=> {
                                 return false;
                             })
@@ -166,7 +146,36 @@
             },
             reset() {
                 this.$refs['forms'].resetFields();
+            },
+            setImgArray(data) {
+                //接收图片选择子组件反回用户选中的图片数组信息
+                this.rule.photo = data.toString();
+            },
+            onEditorReady(editor) { },// 准备编辑器
+            onEditorBlur(){}, // 失去焦点事件
+            onEditorFocus(){}, // 获得焦点事件
+            onEditorChange(){}, // 内容改变事件
+            saveHtml:function(event){
+            alert(this.content);
             }
-        }
+            },
+        components: {Resource}
     }
 </script>
+<style>
+.ql-editor{height:400px;}
+.ql-editor-class {
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        line-height: 1.42;
+        height: 100%;
+        outline: none;
+        padding: 0 !important;
+        tab-size: 4;
+        -moz-tab-size: 4;
+        text-align: left;
+        word-wrap: break-word;
+    }
+/* .ql-snow .ql-color-picker .ql-picker-label svg, .ql-snow .ql-icon-picker .ql-picker-label svg{vertical-align: top;}
+.ql-snow .ql-picker-label::before{vertical-align: top;} */
+</style>
